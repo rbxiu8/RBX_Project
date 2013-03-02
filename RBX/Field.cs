75cdb9ce6e9d97e37758424361720fde1443e01e
Sergey Perfilev <andgo75@gmail.com>
2013-03-02 12:20:10 +0400

    v1.0_creating_constructor_for_Field

--- RBX/Field.cs
+++ RBX/Field.cs
@@ -10,20 +10,30 @@ namespace RBX
     public class Field
     {
         public Int32 width { get; set; }
-        public Int32 heigth { get; set; }
-        public Point center { get; set; }
-        public Point corner_left_top { get; set; }
-        public Point corner_rigth_top { get; set; }
-        public Point corner_left_bottom { get; set; }
-        public Point corner_rigth_bottom { get; set; }
-        public Point side_left_center { get; set; }
-        public Point side_top_center { get; set; }
-        public Point side_rigth_center { get; set; }
-        public Point side_bottom_center { get; set; }
+        public Int32 height { get; set; }
+        public Vector center { get; set; }
+        public Vector corner_left_top { get; set; }
+        public Vector corner_right_top { get; set; }
+        public Vector corner_left_bottom { get; set; }
+        public Vector corner_rigth_bottom { get; set; }
+        public Vector side_left_center { get; set; }
+        public Vector side_top_center { get; set; }
+        public Vector side_rigth_center { get; set; }
+        public Vector side_bottom_center { get; set; }
 
-        public Field(Int32 field_width, Int32 field_heigth)
+        public Field(Int32 field_width, Int32 field_height)
         {
+          center = new Vector(width / 2, height / 2);
+          corner_left_top = new Vector(0,0);
+            corner_right_top = new Vector(width, 0);
+            corner_left_bottom = new Vector(0, height);
+            corner_rigth_bottom = new Vector(width, height);
+           side_left_center = new Vector(0, height / 2);
+          side_rigth_center = new Vector(width, height / 2);
+          side_bottom_center = new Vector(width / 2, height);
+           side_top_center = new Vector(width / 2, 0);
+    	}
 
         }
-    }
+    
 }
