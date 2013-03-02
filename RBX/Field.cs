164dbceed9101da7e74cfc6ba2f76e0c6d6e7df4
Sergey Perfilev <andgo75@gmail.com>
2013-03-02 12:05:21 +0400

    v1.0_correctingfield
    Удалил префиксы свойств в классе Field

--- RBX/Field.cs
+++ RBX/Field.cs
@@ -9,17 +9,17 @@ namespace RBX
 {
     public class Field
     {
-        public Int32 field_width { get; set; }
-        public Int32 field_heigth { get; set; }
-        public Point field_center { get; set; }
-        public Point field_corner_left_top { get; set; }
-        public Point field_corner_rigth_top { get; set; }
-        public Point field_corner_left_bottom { get; set; }
-        public Point field_corner_rigth_bottom { get; set; }
-        public Point field_side_left_center { get; set; }
-        public Point field_side_top_center { get; set; }
-        public Point field_side_rigth_center { get; set; }
-        public Point field_side_bottom_center { get; set; }
+        public Int32 width { get; set; }
+        public Int32 heigth { get; set; }
+        public Point center { get; set; }
+        public Point corner_left_top { get; set; }
+        public Point corner_rigth_top { get; set; }
+        public Point corner_left_bottom { get; set; }
+        public Point corner_rigth_bottom { get; set; }
+        public Point side_left_center { get; set; }
+        public Point side_top_center { get; set; }
+        public Point side_rigth_center { get; set; }
+        public Point side_bottom_center { get; set; }
 
         public Field(Int32 field_width, Int32 field_heigth)
         {
